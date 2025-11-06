461062af06a4038398b792cd0f2398874c9cb05e
Uirá Cardoso <88112570+U1R4@users.noreply.github.com>
2025-11-06 16:25:09 -0300

    Feat: implementado a model tarefa #1

--- src/app.ts
+++ src/app.ts
@@ -4,28 +4,6 @@ import { Request, Response, NextFunction } from "express";
 const app = express();
 app.use(express.json());
 
-//Rotas
-app.get("/params/:name", (req: Request, res: Response, next: NextFunction) => {
-  const name = req.params.name;
-  console.log("Cliente:", name);
-  res.send(`Nome: ${name}`);
-});
 
-app.get("/query", (req: Request, res: Response, next: NextFunction) => {
-  const name = req.query.name;
-  console.log("Cliente digitou:", name);
-  res.send(`Nome: ${name}`);
-});
-
-app.get("/body", (req: Request, res: Response, next: NextFunction) => {
-  const name = req.body.name;
-  console.log("Var dentro do body", name);
-  res.send(`Var dentro do body: ${name}`);
-});
-
-app.get("/exemplo", (req: Request, res: Response, next: NextFunction) => {
-  console.log("aconteceu algo!!!!!");
-  res.send("Ez");
-});
 
 export default app;
---
+++ src/models/task .ts
@@ -0,0 +1,14 @@
+class Task{
+  id: number;
+  name: string;
+  description: string;
+
+  private static nextID = 1;
+
+  constructor(name:string,description: string){
+    this.id = Task.nextID++;
+    this.name = name;
+    this.description = description;
+  }
+
+}
