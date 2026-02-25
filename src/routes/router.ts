d6452a7801fe0885d5dfa24dab0ca2e57e5a72af
Uirá Cardoso <uira.guepardo@gmail.com>
2026-02-25 16:34:38 -0300

    Agora foi

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,28 +1,13 @@
 import { Router } from "express";
-import routeTask from "./task"
 import routeLogin from "./login"
-import {createJWT} from "../utils/jwt";
 import { middleware } from "./jwtMiddleware";
 import routeRoom from "./room";
 import reserveRoute from "./reserve";
 
 const handlerRouter = Router();
 
-handlerRouter.use("/task", routeTask);
 handlerRouter.use("/api/login", routeLogin);
 handlerRouter.use("/api/room", routeRoom)
 handlerRouter.use("/api/reserve", middleware, reserveRoute)
-
-
-
-
-handlerRouter.use("/jwt", (req, res) => {
-    const payload = {id: 123, nome: "teste", cargo: "cliente"}
-    res.json(createJWT(payload))
-});
-
-handlerRouter.get("/testJWT", middleware, (req,res) =>{
-    res.json("passou pelo JWT")
-});
 
 export default handlerRouter;
